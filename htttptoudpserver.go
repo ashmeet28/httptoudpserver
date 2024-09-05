67f9f1c62c5d557189ac5807a018088d326479e9
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2024-09-04 20:49:12 -0400

    Added udp

--- htttptoudpserver.go
+++ htttptoudpserver.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
+	httpToUdpCh := make(chan string)
+
 	http.HandleFunc("GET /sendpacket", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Access-Control-Allow-Origin", "*")
 		w.Header().Add("Access-Control-Allow-Headers", "*")
@@ -14,11 +19,12 @@ func main() {
 		w.Header().Add("Access-Control-Expose-Headers", "*")
 
 		if r.Method == http.MethodGet && r.URL.Path == "/sendpacket" {
+			p := r.Header.Get("httptoudpserver-content")
 
-			d := r.Header.Get("httptoudpserver-content")
-			if d != "" {
-				fmt.Println(d)
+			if p != "" {
+				httpToUdpCh <- p
 			}
+
 			w.WriteHeader(http.StatusOK)
 		} else {
 			w.WriteHeader(http.StatusNotFound)
@@ -37,6 +43,28 @@ func main() {
 			w.WriteHeader(http.StatusNotFound)
 		}
 	})
+
+	go func() {
+		c, err := net.Dial("udp", "127.0.0.1:4242")
+
+		if err != nil {
+			os.Exit(1)
+		}
+
+		var nextPacketId uint64 = 1
+
+		for {
+			c.Write([]byte(((<-httpToUdpCh) + "-" + strconv.FormatUint(nextPacketId, 10))))
+			nextPacketId++
+		}
+	}()
+
+	go func() {
+		for {
+			time.Sleep(time.Second * 1)
+			httpToUdpCh <- "0-0-0-0-0-0-0-0-0"
+		}
+	}()
 
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
